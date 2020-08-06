98ddeafdab017398a314b509e9467f9d5c0c2c2f
buzzlove <buzzlove@mail.ru>
2020-08-06 13:36:01 +0500

    Updates

---
+++ precache-manifest.6f1e0dfbeff2bbb840ee569f4efa689e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bd732344b4e0cc8820711cb30d672c",
+    "url": "/personal-office/index.html"
+  },
+  {
+    "revision": "4179b170e4fa053c6741",
+    "url": "/personal-office/static/css/main.da66608e.chunk.css"
+  },
+  {
+    "revision": "bd9a350f999ad54a1d5a",
+    "url": "/personal-office/static/js/2.d1945892.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-office/static/js/2.d1945892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4179b170e4fa053c6741",
+    "url": "/personal-office/static/js/main.4d61ec49.chunk.js"
+  },
+  {
+    "revision": "845ccae63a947c892694",
+    "url": "/personal-office/static/js/runtime-main.89aa170f.js"
+  }
+]);
